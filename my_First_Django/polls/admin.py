552f1697670525f1e5a862f6a94ecfa1d81063d2
Clifford Gama <cliffygamy@gmail.com>
2020-01-15 00:47:12 +0200

    Finished tutorial seven. Now moving on to section on reusable apps.

--- my_First_Django/polls/admin.py
+++ my_First_Django/polls/admin.py
@@ -1,7 +1,18 @@
 from django.contrib import admin
 from .models import Question, Choice
 
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
 
-admin.site.register(Question)
-admin.site.register(Choice)
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['question_text']}),
+        ('Date Information', {'fields': ['publication_date'], 'classes': ['collapse']}),
+        ]
+    inlines = [ChoiceInline]
+    list_display = ['question_text', 'publication_date', 'was_published_recently']
+    list_filter = ['publication_date']
+    search_fields = ['question_text']
+admin.site.register(Question, QuestionAdmin)
 # Register your models here.
